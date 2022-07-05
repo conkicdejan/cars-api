b4d1f25d3ae30ba95b3ffa7583f9f53b4dac8bff
Dejan Conkic <conkicdejan@gmail.com>
2022-07-05 19:14:41 +0200

    Task 2

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCarRequest;
+use App\Http\Requests\UpdateCarRequest;
+use App\Models\Car;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class CarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cars = Car::all();
+        return response()->json($cars);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCarRequest $request)
+    {
+        $validated = $request->validated();
+        $cars = Car::create($validated);
+        return response()->json($cars);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Car $car)
+    {
+        return response()->json($car);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCarRequest $request, Car $car)
+    {
+        $validated = $request->validated();
+        $car->update($validated);
+        return response()->json($car);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Car $car)
+    {
+        $car->delete();
+        return response("Car {$car->brand} {$car->model} deleted");
+    }
+}
---
+++ app/Http/Requests/UpdateCarRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCarRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'brand' => 'required|string|max:255',
+            'model' => 'required|string|max:255',
+            'year' => 'sometimes|integer|between:1950,' . now()->format('Y'),
+            'max_speed' => 'sometimes|integer|between:0,350',
+            'is_automatic' => 'sometimes|boolean|',
+            'engine' => 'sometimes|integer|between:750,6000',
+            'number_of_doors' => 'sometimes|integer|between:1,5',
+        ];
+    }
+}
